de0ec8a57da1c28bb8491eebdaed07d148226b61
sokdararith <praksokdararith@outlook.com>
2021-09-16 13:55:58 +0700

    change process to add meter

--- customer/addMeter.js
+++ customer/addMeter.js
@@ -1,7 +1,8 @@
 const response = require('../src/response')
 const db = require('../src/db')
 const { v1: uuid } = require('uuid');
-const dbClient = db.dbClient('us-east-1')
+// const dbClient = db.dbClient('us-east-1')
+const dbClient = db.localhost();
 
 module.exports.index = async (event) => {
     if (event.headers["Content-Type"] !== "application/json") {
@@ -19,20 +20,21 @@ module.exports.index = async (event) => {
     this.data = JSON.parse(event.body)
     this.uuid = uuid()
     this.createdAt = new Date().toISOString()
-    this.table = "Entity-dev"
+    this.table = "Entity"
     this.meter = {
         pk: `meter-${this.uuid}`,
         sk: this.data.customer.id,
-        "gsi1-pk": this.instId,
+        "gsi1-pk": `inst-${this.instId}`,
         "gsi1-sk": `meter-${this.uuid}`
     }
     this.params = {
         TableName: this.table,
         Item: this.meter
     }
-
+    console.log(this.params)
     try {
         const result = await dbClient.put(this.params).promise()
+        return response.success(this.meter)
     } catch(err) {
         return response.failure({ error: err })
     }
